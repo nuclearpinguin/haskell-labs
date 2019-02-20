b4458985f0d1d5f69cdeca43d30f28b5bfde6f62
nuclearpinguin <turbaszek@gmail.com>
2019-02-20 20:09:46 +0100

    no message

--- factor.hs
+++ factor.hs
@@ -1,3 +1,18 @@
 factor :: Integer -> Integer
 factor 0 = 1
 factor n = n * factor (n-1)
+
+
+myReverse :: [a] -> [a]
+myReverse (x:xs) = myReverse(xs) ++ [x]
+myReverse [] = []
+
+-- isPalindrome :: Eq a => [a] -> Bool
+-- isPalindrome a = a == reverse a
+
+-- listLen :: [a] -> Integer
+-- listLen (_:xs) = 1 + listLen(xs)
+-- listLen [] = 0
+
+-- makePalindrome :: [a] -> [a]
+-- makePalindrome xs = xs ++ reverse(xs)
