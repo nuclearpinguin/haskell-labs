22d804fcb51a58285b18ed47f8ff2f9de91de953
nuclearpinguin <turbaszek@gmail.com>
2019-02-21 21:46:14 +0100

    Some works

--- Labs/lab1.hs
+++ Labs/lab1.hs
@@ -28,4 +28,47 @@ fibSeq 0 = 1
 fibSeq 1 = 1
 fibSeq n = fibSeq(n-1) + fibSeq(n-2)
 
+factor :: Integer -> Integer
+factor 0 = 1
+factor n = n * factor (n-1)
 
+tylor1 :: Double -> Integer -> Double
+tylor1 x n 
+        | n > 0 = x ^ (2*n + 1) * (-1)^n / fromInteger(factor (2*n + 1) ) + tylor1 x (n-1)
+        | n == 0 = x
+
+-- using the last element of series and present index i 
+-- it evaluates next element for a given x 
+tylorSinNext :: Double -> Integer -> Double -> Double
+tylorSinNext x i last = (last * x^2)  * (-1) /  fromIntegral( (i+1) * (i+2) )
+
+-- helper for tylorSeries
+tylorAdderSeries :: Double -> Integer -> Integer -> [Double] -> Double -> [Double]
+tylorAdderSeries x i n s last 
+    | i <= n+1 = tylorAdderSeries x (i+2) n new_s new_last
+    | otherwise = s
+    where
+        new_last = tylorSinNext x i last
+        new_s = s ++ [new_last]
+
+-- creates list of first n elements of Tylor series of sin x
+tylorSeries :: Double -> Integer -> [Double]
+tylorSeries x n = tylorAdderSeries x 1 n [x] x
+
+
+tylorSumSeries :: Double -> Integer -> Double
+tylorSumSeries x n = sum $ tylorSeries x n 
+
+
+-- helper for tylorSin
+tylorAdder :: Double -> Integer -> Integer -> Double -> Double -> Double
+tylorAdder x i n s last 
+    | i <= n+1 = tylorAdder x (i+2) n new_s new_last
+    | otherwise = s
+    where
+        new_last = tylorSinNext x i last
+        new_s = s + new_last
+
+-- creates list of first n elements of Tylor series of sin x
+tylorSin :: Double -> Integer -> Double
+tylorSin x n = tylorAdder x 1 n x x
---
+++ Labs/taylor.hs
@@ -0,0 +1,47 @@
+module Taylor (taylorSin, taylorSinSeries) where
+
+-- Eager Taylor
+-- taylor1 :: Double -> Integer -> Double
+-- taylor1 x n 
+--         | n > 0 = x ^ (2*n + 1) * (-1)^n / fromInteger(factor (2*n + 1) ) + taylor1 x (n-1)
+--         | n == 0 = x
+
+
+-- Creates list of first n elements of Taylor series of sin x
+taylorSinSeries :: Double -> Integer -> [Double]
+taylorSinSeries x n = taylorConcat x 1 n [x] x
+
+
+-- Helper for taylorSinSeries
+taylorConcat :: Double -> Integer -> Integer -> [Double] -> Double -> [Double]
+taylorConcat x i n s last 
+    | i <= n+1 = taylorConcat x (i+2) n new_s new_last
+    | otherwise = s
+    where
+        new_last = taylorSinNext x i last
+        new_s = s ++ [new_last]
+
+-- Oh well...
+-- taylorSumSeries :: Double -> Integer -> Double
+-- taylorSumSeries x n = sum $ taylorConcat x n 
+
+
+-- Creates list of first n elements of Taylor series of sin x
+taylorSin :: Double -> Integer -> Double
+taylorSin x n = taylorAdder x 1 n x x
+
+
+-- Helper for taylorSin
+taylorAdder :: Double -> Integer -> Integer -> Double -> Double -> Double
+taylorAdder x i n s last 
+    | i <= n+1 = taylorAdder x (i+2) n new_s new_last
+    | otherwise = s
+    where
+        new_last = taylorSinNext x i last
+        new_s = s + new_last
+
+
+-- Using the last element of series and present index i 
+-- it evaluates next element for a given x 
+taylorSinNext :: Double -> Integer -> Double -> Double
+taylorSinNext x i last = (last * x^2)  * (-1) /  fromIntegral( (i+1) * (i+2) )
