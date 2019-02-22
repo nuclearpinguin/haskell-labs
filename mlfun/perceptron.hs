9b9f40829bd7d98794dbd5a71d9d405e3953e111
nuclearpinguin <turbaszek@gmail.com>
2019-02-22 20:14:56 +0100

    Working perceptron

--- mlfun/perceptron.hs
+++ mlfun/perceptron.hs
@@ -1,28 +1,41 @@
--- Rosenblatt Perceptron
+module Perceptron (fit, predict, score, dot, add) where
+    
+-- ROSENBLATT PERCEPTRON
 
--- Dot product of two lists
-dot :: [Double] -> [Double] -> Double
-dot xs ys 
-    | length xs > 0 && length ys > 0 = (head xs) * (head ys) + dot (tail xs) (tail ys) 
-    | otherwise = 0
-
-
-net_input :: [Double] -> [Double] -> Double
-net_input xs (w:ws) = w + dot xs ws 
- 
+-- Given an vectors of features and perceptron weights it predict the class (1, 0)
 predict :: [Double] -> [Double] -> Double 
-predict xs ws
-    | val >= 0 = 1.0
-    | val < 0 = -1.0
+predict (w:ws) xs 
+    | val >= 0.0 = 1.0
+    | val < 0.0 = 0.0
     where
-        val = net_input xs ws
+        val = w + dot xs ws 
 
+-- Executes n epochs of updataing weight on a given batch
+fit :: [[Double]] -> [Double] -> Double -> Integer -> [Double]
+fit xs ys rate n 
+    |(length xs) == (length ys) = fitHelper 1 n rate init_w xs ys
+    | otherwise = init_w
+    where
+        init_w = replicate (length (xs!!0) + 1) 0.0
+
+
+-- Helper for fitting
+fitHelper :: Integer -> Integer -> Double -> [Double] -> [[Double]] -> [Double] -> [Double]
+fitHelper i n rate ws xs ys 
+    | i <= n = fitHelper (i+1) n rate new_w xs ys
+    | otherwise = ws
+    where
+        new_w = updateWeightsBatch rate ws xs ys
+
+    
+-- Updates weights for a single observation (X, y) 
 updateWeightsSingle :: Double -> [Double] -> [Double] -> Double -> [Double]
-updateWeightsSingle rate (w:ws) xs y = (w + update) : map (update*) xs
+updateWeightsSingle rate (w:ws) xs y = [w + update] ++ add (map (update*) xs) ws
     where
-        update = rate * ( y - predict xs ws)
+        update = rate * ( y - predict ws xs)
 
 
+-- Updates weights for a batch of observations ([[X1], [X2], ...], [y1, y2])
 updateWeightsBatch :: Double -> [Double] -> [[Double]] -> [Double] -> [Double]
 updateWeightsBatch rate (w:ws) (x:xs) (y:ys) 
     | length xs > 0 = updateWeightsBatch rate new_w xs ys
@@ -31,17 +44,37 @@ updateWeightsBatch rate (w:ws) (x:xs) (y:ys)
         new_w = updateWeightsSingle rate (w:ws) x y
 
 
-fit :: Integer -> Integer -> Double -> [Double] -> [[Double]] -> [Double] -> [Double]
-fit i n rate ws xs ys 
-    | i <= n = fit (i+1) n rate new_w xs ys
-    | otherwise = ws
+-- SCORE HELPERS
+        
+-- TP / ALL
+score :: [[Double]] -> [Double] -> [Double] -> Double
+score xs ys ws = 1 - fromIntegral ( abs (y - y_hat) )/ fromIntegral (length ys)
     where
-        new_w = updateWeightsBatch rate ws xs ys
+        m = map (predict ws) xs
+        y = length $ filter (==0.0) ys
+        y_hat = length $ filter (==0.0) m
+
+
+-- LINEAR ALGEBRA HELPERS
+
+-- Dot product of two lists
+dot :: [Double] -> [Double] -> Double
+dot xs ys 
+    | cond = (head xs) * (head ys) + dot (tail xs) (tail ys) 
+    | otherwise = 0
+    where
+        lxs = length xs
+        lys = length ys
+        cond = (lxs == lys) && lxs >0 && lys >0
+
+-- Element-wise sum of two lists
+add :: [Double] -> [Double] -> [Double]
+add (x:xs) (y:ys) 
+    | cond = [x+y] ++ add xs ys
+    | otherwise = [x+y]
+    where
+        lxs = length xs
+        lys = length ys
+        cond = (lxs == lys) && lxs >0 && lys >0
 
         
-test :: [Double]        
-test  = fit 1 100 0.001 ws xs ys
-    where
-        ws = [0.0, 0.0, 0.0]
-        xs = [[0.1, 0.5], [0.4, 0.1], [0.15, 0.37], [0.67, 0.09], [0.1, 0.45], [0.17, 0.39]]
-        ys = [1.0, -1.0, 1.0, -1.0, 1.0, 1.0]
