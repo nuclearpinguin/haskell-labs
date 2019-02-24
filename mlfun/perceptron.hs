cf3cc2919d64567f76be2aa28324a4ef5bdd1a7d
nuclearpinguin <turbaszek@gmail.com>
2019-02-24 22:30:53 +0100

    Simplify things

--- mlfun/perceptron.hs
+++ mlfun/perceptron.hs
@@ -1,4 +1,4 @@
-module Perceptron (fit, predict, score, dot, add) where
+module Perceptron (fit, predict, score, dot) where
     
 -- ROSENBLATT PERCEPTRON
 
@@ -13,7 +13,7 @@ predict (w:ws) xs
 -- Executes n epochs of updataing weight on a given batch
 fit :: [[Double]] -> [Double] -> Double -> Integer -> [Double]
 fit xs ys rate n 
-    |(length xs) == (length ys) = fitHelper 1 n rate init_w xs ys
+    | (length xs) == (length ys) = fitHelper 1 n rate init_w xs ys
     | otherwise = init_w
     where
         init_w = replicate (length (xs!!0) + 1) 0.0
@@ -25,23 +25,17 @@ fitHelper i n rate ws xs ys
     | i <= n = fitHelper (i+1) n rate new_w xs ys
     | otherwise = ws
     where
-        new_w = updateWeightsBatch rate ws xs ys
+        new_w = updateWeights rate ws xs ys
 
     
--- Updates weights for a single observation (X, y) 
-updateWeightsSingle :: Double -> [Double] -> [Double] -> Double -> [Double]
-updateWeightsSingle rate (w:ws) xs y = [w + update] ++ add (map (update*) xs) ws
-    where
-        update = rate * ( y - predict ws xs)
-
-
 -- Updates weights for a batch of observations ([[X1], [X2], ...], [y1, y2])
-updateWeightsBatch :: Double -> [Double] -> [[Double]] -> [Double] -> [Double]
-updateWeightsBatch rate (w:ws) (x:xs) (y:ys) 
-    | length xs > 0 = updateWeightsBatch rate new_w xs ys
-    | otherwise = w:ws
+updateWeights :: Double -> [Double] -> [[Double]] -> [Double] -> [Double]
+updateWeights rate ws [] _ = ws
+updateWeights rate ws _ [] = ws 
+updateWeights rate (w:ws) (x:xs) (y:ys) = updateWeights rate new_w xs ys
     where
-        new_w = updateWeightsSingle rate (w:ws) x y
+        update = rate * ( y - predict ws x)
+        new_w = [w + update] ++ (zipWith (+) (map (update*) x) ws )
 
 
 -- SCORE HELPERS
@@ -59,22 +53,5 @@ score xs ys ws = 1 - fromIntegral ( abs (y - y_hat) )/ fromIntegral (length ys)
 
 -- Dot product of two lists
 dot :: [Double] -> [Double] -> Double
-dot xs ys 
-    | cond = (head xs) * (head ys) + dot (tail xs) (tail ys) 
-    | otherwise = 0
-    where
-        lxs = length xs
-        lys = length ys
-        cond = (lxs == lys) && lxs >0 && lys >0
-
--- Element-wise sum of two lists
-add :: [Double] -> [Double] -> [Double]
-add (x:xs) (y:ys) 
-    | cond = [x+y] ++ add xs ys
-    | otherwise = [x+y]
-    where
-        lxs = length xs
-        lys = length ys
-        cond = (lxs == lys) && lxs >0 && lys >0
-
+dot xs ys = sum $ zipWith (*) xs ys
         
